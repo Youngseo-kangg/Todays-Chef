c8cae91a48196c034fdbcb4b3a83b0457827d462
Lee Naeun <nanni4287@gmail.com>
2021-10-25 15:16:30 +0900

    [Client / Server] server와 client 초기 axios 연결 확인

--- server/app.js
+++ server/app.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { sequelize } = require('./models');
+require('dotenv').config();
 
 const localPort = 4000;
-const remotePort = 80;
+const serverPort = process.env.SERVER_PORT || 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello Server!');
 });
 
-app.listen(remotePort, () => {
+app.use((req, res, next) => {
+  res.status(404).send('Not Found!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({
+    message: 'Internal Server Error',
+    stacktrace: err.toString(),
+  });
+});
+
+// 데이터베이스 연결
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log();
+    console.log(`👍데이터베이스 연결 성공👍 \n`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.listen(serverPort, () => {
   console.log(`서버 연결 성공 🍎`);
 });
