cac103a7d82ba9f0e6385295c34adca0297f57a5
Lee-Na-eun <81851620+Lee-Na-eun@users.noreply.github.com>
2021-11-01 10:22:16 +0900

    Revert "[Client] / #11 / nav에 throttle 및 반응형 구현과 배경 고정"

--- client/src/styled/styleMain.js
+++ client/src/styled/styleMain.js
@@ -15,9 +15,7 @@ export const MainBackgroundWrap = styled.div`
 
 export const MainBackground = styled.video`
   width: 100%;
-  height: 90vh;
-  object-fit: cover;
-  /* margin-top: -200px;
+  margin-top: -200px;
   @media (max-width: 2000px) {
     width: 2000px;
     margin-top: -300px;
@@ -31,64 +29,28 @@ export const MainBackground = styled.video`
     margin-top: 0;
     margin-left: -500px;
     height: 100vh;
-  } */
+  }
 `;
 
 export const SloganWrap = styled.div`
-  width: 100%;
+  width: 800px;
   height: 400px;
   border-radius: 20px;
   position: absolute;
   top: 250px;
   left: 50%;
   transform: translate(-50%);
-  @media (max-width: 420px) {
-    top: 200px;
-  }
-  @media (max-width: 320px) {
-    top: 150px;
-  }
-  @media (max-width: 280px) {
-    top: 230px;
-  }
   > h1 {
     width: 100%;
     height: 150px;
     line-height: 100px;
     font-size: 40px;
     color: #fff;
-    @media (max-width: 767px) {
-      font-size: 30px;
-    }
-    @media (max-width: 420px) {
-      font-size: 28px;
-      height: auto;
-    }
-    @media (max-width: 320px) {
-      font-size: 25px;
-    }
-    @media (max-width: 280px) {
-      font-size: 20px;
-      line-height: 70px;
-    }
   }
   > h2 {
     height: 250px;
     font-size: 25px;
     color: #fff;
     line-height: 50px;
-    @media (max-width: 767px) {
-      font-size: 20px;
-    }
-    @media (max-width: 420px) {
-      font-size: 18px;
-    }
-    @media (max-width: 320px) {
-      font-size: 15px;
-    }
-    @media (max-width: 280px) {
-      font-size: 11px;
-      line-height: 30px;
-    }
   }
 `;
