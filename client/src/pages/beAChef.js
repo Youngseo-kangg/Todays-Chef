e2fe3d1bbac4f57c04df6e755b25cf7ae090278e
Youngseo Kang <youngseo.kangg@gmail.com>
2021-11-04 01:37:52 +0900

    [Client] beAChef 페이지 css 지정 및 반응형 구현

--- client/src/pages/beAChef.js
+++ client/src/pages/beAChef.js
@@ -17,8 +17,53 @@ function BeAChef() {
           </div>
         </div>
       </BeAChefIntro>
-      <BeAChefDesc>BeAChef 설명</BeAChefDesc>
-      <BeAChefResumeWrap>BeAChef 신청하기</BeAChefResumeWrap>
+      <BeAChefDesc>
+        <h3 id='beChefDescTitle'>Todays chef에서 셰프로 활동해 보세요.</h3>
+        <section id='beChefDesc'>
+          <div className='beChefDescText'>
+            <div>
+              <p>셰프의 상상력을 발휘한,</p>
+              <p>세상에 하나뿐인 미식 코스를</p>
+              <p>제공해 보세요.</p>
+            </div>
+          </div>
+          <div className='beChefDescText'>
+            <div>
+              <p>고객 개개인에게 최적화한</p>
+              <p>파인다이닝 서비스 경험을</p>
+              <p>쌓을 수 있습니다.</p>
+            </div>
+          </div>
+          <div className='beChefDescText'>
+            <div>
+              <p>이력서를 등록하고 24~72시간 후</p>
+              <p>고객센터에서 셰프 선정 여부를</p>
+              <p>안내해 드립니다.</p>
+            </div>
+          </div>
+        </section>
+      </BeAChefDesc>
+      <BeAChefResumeWrap>
+        <h3 id='resumeTitle'>셰프 신청 하기</h3>
+        <p>
+          자유 형식의 이력서, 자기소개서 및 경력증빙서류를 하나의 pdf로 제출해
+          주시면 됩니다.
+        </p>
+        <div id='resumeFormWrap'>
+          <form>
+            <select name='resumeCuisine' id='resumeCuisine'>
+              <option>한식</option>
+              <option>일식</option>
+              <option>중식</option>
+              <option>양식</option>
+            </select>
+            <input type='file' name='resumeFile' id='resumeFile' />
+            <button type='submit' id='submitBtn'>
+              제출
+            </button>
+          </form>
+        </div>
+      </BeAChefResumeWrap>
     </BeAChefGrid>
   );
 }
--- client/src/styled/styleBeAChef.js
+++ client/src/styled/styleBeAChef.js
@@ -1,19 +1,24 @@
 import styled from 'styled-components';
 import beAChefBackground from '../todaysChefIMG/beAChefBackground.jpg';
+import beAChefDesc1 from '../todaysChefIMG/beAChefDesc1.jpg';
+import beAChefDesc2 from '../todaysChefIMG/beAChefDesc2.jpg';
+import beAChefDesc3 from '../todaysChefIMG/beAChefDesc3.jpg';
 
 export const BeAChefGrid = styled.article`
   width: 100%;
-  padding: 80px 0px 50px;
+  padding-bottom: 50px;
   display: grid;
-  grid-template-rows: 550px 550px 300px;
+  grid-template-rows: 630px 500px 300px;
   grid-row-gap: 50px;
+  min-width: 280px;
   > section {
-    border: 1px solid red;
+    /* border: 1px solid red; */
   }
   @media (max-width: 767px) {
     > section {
-      border: 1px solid blue;
+      /* border: 1px solid blue; */
     }
+    grid-template-rows: 630px 1352px 350px;
   }
 `;
 
@@ -41,13 +46,113 @@ export const BeAChefIntro = styled.section`
     flex-direction: column;
     h3 {
       font-size: 23px;
-      padding-bottom: 15px;
+      padding: 7.5px 0px;
     }
-    p:nth-child(1) {
+    p {
       font-size: 18px;
-      padding-bottom: 10px;
+      padding: 5px 0px;
     }
   }
 `;
-export const BeAChefDesc = styled.section``;
-export const BeAChefResumeWrap = styled.section``;
+
+export const BeAChefDesc = styled.section`
+  display: grid;
+  grid-template-rows: 74px 426px;
+  background-color: #fff;
+  h3 {
+    font-size: 23px;
+    display: flex;
+    text-align: left;
+    justify-content: flex-start;
+    align-items: center;
+    word-break: keep-all;
+  }
+  #beChefDesc {
+    display: flex;
+    flex-direction: row;
+    .beChefDescText {
+      width: calc(100% / 3);
+      min-width: 255px;
+      /* border: 1px solid red; */
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+      :nth-child(1) {
+        background: url(${beAChefDesc1}) no-repeat center;
+      }
+      :nth-child(2) {
+        background: url(${beAChefDesc2}) no-repeat center;
+      }
+      :nth-child(3) {
+        background: url(${beAChefDesc3}) no-repeat center;
+      }
+      > div {
+        background: rgba(255, 255, 255, 0.7);
+        width: 100%;
+        height: 100px;
+        padding: 10px;
+        display: grid;
+        place-items: center;
+        word-break: keep-all;
+      }
+    }
+  }
+  @media (max-width: 767px) {
+    grid-template-rows: 74px 1278px;
+    #beChefDesc {
+      flex-direction: column;
+      .beChefDescText {
+        width: 100%;
+        height: 426px;
+      }
+    }
+  }
+`;
+
+export const BeAChefResumeWrap = styled.section`
+  display: grid;
+  grid-template-rows: 73px 60px 217px;
+  background-color: #fff;
+  h3 {
+    font-size: 23px;
+    display: flex;
+    text-align: left;
+    justify-content: flex-start;
+    align-items: center;
+    word-break: keep-all;
+  }
+  p {
+    display: flex;
+    text-align: left;
+    justify-content: flex-start;
+    align-items: center;
+    word-break: keep-all;
+  }
+  #resumeFormWrap {
+    display: grid;
+    place-items: center;
+    background-color: #603224;
+    > form {
+      display: block;
+      width: 80%;
+      min-width: 280px;
+      height: 60px;
+      background-color: rgba(255, 255, 255, 0.3);
+      border-radius: 5px;
+      display: grid;
+      grid-template-columns: 1fr 3fr 1fr;
+      grid-template-rows: 30px;
+      padding: 15px;
+      @media (max-width: 420px) {
+        height: 100px;
+        grid-template-columns: none;
+        grid-template-rows: 1fr 1fr 1fr;
+        select,
+        input,
+        button {
+          width: 100%;
+        }
+      }
+    }
+  }
+`;
