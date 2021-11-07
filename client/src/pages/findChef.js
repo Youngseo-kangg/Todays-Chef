69e4c23af05b5d29b5a9de10cd27870015ede6ea
Youngseo Kang <youngseo.kangg@gmail.com>
2021-11-07 18:02:18 +0900

    [Client] findChef 구조와 css 수정

--- client/src/pages/findChef.js
+++ client/src/pages/findChef.js
@@ -1,5 +1,13 @@
 import { Link } from 'react-router-dom';
-import { FindChefGrid, SelectCuisine, ChefList } from '../styled/styleFindChef';
+import basic_profile from '../todaysChefIMG/basic_profile.jpeg';
+import {
+  FindChefGrid,
+  SelectCuisine,
+  ChefList,
+  ChefListTitleWrap,
+  ChefItemList,
+  PagenationList,
+} from '../styled/styleFindChef';
 
 function FindChef() {
   return (
@@ -7,29 +15,81 @@ function FindChef() {
       <SelectCuisine>
         <div id='cuisineChoiceWrap'>
           <ul>
-            <li>한식</li>
-            <li>일식</li>
-          </ul>
-          <ul>
-            <li>양식</li>
-            <li>중식</li>
+            <li>
+              <img />
+              <h2>한식</h2>
+            </li>
+            <li>
+              <img />
+              <h2>일식</h2>
+            </li>
+            <li>
+              <img />
+              <h2>중식</h2>
+            </li>
+            <li>
+              <img />
+              <h2>양식</h2>
+            </li>
           </ul>
         </div>
       </SelectCuisine>
 
       <ChefList>
-        <p>셰프님들 선택</p>
-        <ul>
-          <li>
-            <Link to='/chefInfo'>1번 chefInfo</Link>
-          </li>
-          <li>
-            <Link to='/chefInfo'>2번 chefInfo</Link>
-          </li>
-          <li>
-            <Link to='/chefInfo'>3번 chefInfo</Link>
-          </li>
-        </ul>
+        <div id='chefListWrap'>
+          <ChefListTitleWrap>
+            <h2>한식 셰프</h2>
+            <select name='findChefCuisine' id='findChefCuisine'>
+              <option value='kr'>한식</option>
+              <option value='jp'>일식</option>
+              <option value='ch'>중식</option>
+              <option value='eu'>양식</option>
+            </select>
+          </ChefListTitleWrap>
+
+          <ChefItemList>
+            <ul>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>김코딩 셰프</Link>
+                </h3>
+                <span>4.7</span>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>박코딩 셰프</Link>
+                </h3>
+                <span>4.8</span>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>이코딩 셰프</Link>
+                </h3>
+                <span>4.7</span>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>최코딩 셰프</Link>
+                </h3>
+                <span>4.7</span>
+              </li>
+            </ul>
+          </ChefItemList>
+
+          <PagenationList>ararar</PagenationList>
+        </div>
       </ChefList>
     </FindChefGrid>
   );
--- client/src/styled/styleFindChef.js
+++ client/src/styled/styleFindChef.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-export const FindChefGrid = styled.div`
+export const FindChefGrid = styled.article`
   padding-top: 80px;
   display: grid;
-  grid-template-rows: 200px 750px;
+  grid-template-rows: 200px 510px;
   row-gap: 50px;
   margin-bottom: 50px;
   @media (max-width: 767px) {
-    /* grid-template-rows: 250px 750px; */
+    grid-template-rows: 250px 940px;
   }
   @media (max-width: 430px) {
-    grid-template-rows: 250px 750px;
+    grid-template-rows: 250px 1770px;
   }
 `;
 
@@ -20,8 +20,10 @@ export const SelectCuisine = styled.div`
   place-items: center;
   #cuisineChoiceWrap {
     width: 90%;
+    max-width: 1400px;
     height: 80px;
     border: 1px solid #fff;
+    box-sizing: border-box;
     display: flex;
     @media (max-width: 767px) {
       height: 160px;
@@ -34,30 +36,124 @@ export const SelectCuisine = styled.div`
       display: flex;
       flex-wrap: wrap;
       justify-content: space-evenly;
-      border: 1px solid blue;
       width: 100%;
       height: 100%;
-      @media (max-width: 767px) {
-        :nth-child(1) {
-          margin-bottom: 10px;
-        }
-      }
       @media (max-width: 430px) {
         :nth-child(1) {
           margin-bottom: 0px;
         }
       }
+
       li {
         border: 1px solid blue;
-        width: calc(100% / 2 - 20px);
+        box-sizing: border-box;
+        width: calc(100% / 4 - 20px);
+        @media (max-width: 767px) {
+          width: calc(100% / 2 - 10px);
+          :nth-child(1),
+          :nth-child(2) {
+            margin-bottom: 10px;
+          }
+        }
         @media (max-width: 430px) {
           width: 100%;
+          :nth-child(1),
+          :nth-child(2) {
+            margin-bottom: 0px;
+          }
         }
       }
     }
   }
 `;
 
-export const ChefList = styled.div`
+export const ChefList = styled.section`
   border: 1px solid red;
+  box-sizing: border-box;
+  display: grid;
+  place-items: center;
+  #chefListWrap {
+    border: 1px solid red;
+    box-sizing: border-box;
+    width: 90%;
+    max-width: 1400px;
+    display: grid;
+    grid-template-rows: 30px 420px 30px;
+    grid-row-gap: 15px;
+    @media (max-width: 767px) {
+      grid-template-rows: 30px 850px 30px;
+    }
+    @media (max-width: 430px) {
+      width: 100%;
+      grid-template-rows: 30px 1680px 30px;
+    }
+  }
+`;
+
+export const ChefListTitleWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  > h2 {
+    font-size: 30px;
+    text-align: left;
+  }
+  > select {
+    width: 100px;
+    border: none;
+    border-radius: 5px;
+  }
+`;
+
+export const ChefItemList = styled.div`
+  display: flex;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    width: 100%;
+    height: 100%;
+    @media (max-width: 430px) {
+      :nth-child(1) {
+        margin-bottom: 0px;
+      }
+    }
+    li {
+      border: 1px solid blue;
+      box-sizing: border-box;
+      width: calc(100% / 4 - 20px);
+      display: grid;
+      grid-template-rows: 8fr 1fr 1fr;
+      @media (max-width: 767px) {
+        width: calc(100% / 2 - 10px);
+        :nth-child(1),
+        :nth-child(2) {
+          margin-bottom: 10px;
+        }
+      }
+      @media (max-width: 430px) {
+        width: 100%;
+        :nth-child(1),
+        :nth-child(2) {
+          margin-bottom: 0px;
+        }
+      }
+      > .chefPic {
+        border: 1px solid red;
+        display: grid;
+        place-items: center;
+        img {
+          width: 80%;
+          height: 80%;
+          border-radius: 100%;
+        }
+      }
+    }
+  }
+`;
+
+export const PagenationList = styled.div`
+  border: 1px solid red;
+  width: 100%;
+  max-width: 1400px;
+  height: 100%;
 `;
