5a23ae09d2291125cdd89d8fb722aa51a91932c0
Youngseo Kang <youngseo.kangg@gmail.com>
2021-11-08 17:56:11 +0900

    [Client] findChef css 작성

--- client/src/pages/findChef.js
+++ client/src/pages/findChef.js
@@ -1,35 +1,63 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import basic_profile from '../todaysChefIMG/basic_profile.jpeg';
+import koreanFood from '../todaysChefIMG/foodKorean.png';
+import japaneseFood from '../todaysChefIMG/foodJapanese.png';
+import italianFood from '../todaysChefIMG/foodItalian.png';
+import chineseFood from '../todaysChefIMG/foodChinese.png';
+
 import {
   FindChefGrid,
   SelectCuisine,
   ChefList,
   ChefListTitleWrap,
   ChefItemList,
+  ChefStar,
   PagenationList,
 } from '../styled/styleFindChef';
 
 function FindChef() {
+  const [selected, setSelected] = useState('한식');
+  const handleSelected = (event) => {
+    if (event.target.value === 'kr') {
+      setSelected('한식');
+    } else if (event.target.value === 'jp') {
+      setSelected('일식');
+    } else if (event.target.value === 'ch') {
+      setSelected('중식');
+    } else if (event.target.value === 'eu') {
+      setSelected('양식');
+    }
+  }; // select에서 선택하는대로 어떤 퀴진 셰프인지 변경해주는 함수
+
   return (
     <FindChefGrid>
       <SelectCuisine>
         <div id='cuisineChoiceWrap'>
           <ul>
             <li>
-              <img />
-              <h2>한식</h2>
+              <div className='cuisineChoice'>
+                <img src={koreanFood} alt='한식' />
+                <h2>한식</h2>
+              </div>
             </li>
             <li>
-              <img />
-              <h2>일식</h2>
+              <div className='cuisineChoice'>
+                <img src={japaneseFood} alt='일식' />
+                <h2>일식</h2>
+              </div>
             </li>
             <li>
-              <img />
-              <h2>중식</h2>
+              <div className='cuisineChoice'>
+                <img src={chineseFood} alt='중식' />
+                <h2>중식</h2>
+              </div>
             </li>
             <li>
-              <img />
-              <h2>양식</h2>
+              <div className='cuisineChoice'>
+                <img src={italianFood} alt='양식' />
+                <h2>양식</h2>
+              </div>
             </li>
           </ul>
         </div>
@@ -38,8 +66,12 @@ function FindChef() {
       <ChefList>
         <div id='chefListWrap'>
           <ChefListTitleWrap>
-            <h2>한식 셰프</h2>
-            <select name='findChefCuisine' id='findChefCuisine'>
+            <h2>{selected} 셰프</h2>
+            <select
+              name='findChefCuisine'
+              id='findChefCuisine'
+              onChange={handleSelected}
+            >
               <option value='kr'>한식</option>
               <option value='jp'>일식</option>
               <option value='ch'>중식</option>
@@ -56,7 +88,8 @@ function FindChef() {
                 <h3>
                   <Link to='/chefInfo'>김코딩 셰프</Link>
                 </h3>
-                <span>4.7</span>
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
               </li>
               <li className='chef'>
                 <div className='chefPic'>
@@ -65,7 +98,9 @@ function FindChef() {
                 <h3>
                   <Link to='/chefInfo'>박코딩 셰프</Link>
                 </h3>
-                <span>4.8</span>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
               </li>
               <li className='chef'>
                 <div className='chefPic'>
@@ -74,7 +109,9 @@ function FindChef() {
                 <h3>
                   <Link to='/chefInfo'>이코딩 셰프</Link>
                 </h3>
-                <span>4.7</span>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
               </li>
               <li className='chef'>
                 <div className='chefPic'>
@@ -83,12 +120,68 @@ function FindChef() {
                 <h3>
                   <Link to='/chefInfo'>최코딩 셰프</Link>
                 </h3>
-                <span>4.7</span>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>김코딩 셰프</Link>
+                </h3>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>박코딩 셰프</Link>
+                </h3>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>이코딩 셰프</Link>
+                </h3>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
+              </li>
+              <li className='chef'>
+                <div className='chefPic'>
+                  <img src={basic_profile} alt='셰프 사진' />
+                </div>
+                <h3>
+                  <Link to='/chefInfo'>최코딩 셰프</Link>
+                </h3>
+
+                <span>4.9</span>
+                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
               </li>
             </ul>
           </ChefItemList>
 
-          <PagenationList>ararar</PagenationList>
+          <PagenationList>
+            <ul>
+              <li>&lt;&lt;</li>
+              <li>&lt;</li>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>&gt;</li>
+              <li>&gt;&gt;</li>
+            </ul>
+          </PagenationList>
         </div>
       </ChefList>
     </FindChefGrid>
