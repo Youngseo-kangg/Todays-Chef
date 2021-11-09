9e513a4c7cfa9a4ec594627edd484c5b8a1f69a9
Youngseo Kang <youngseo.kangg@gmail.com>
2021-11-09 16:54:01 +0900

    [Client] chefInfo html 태그 및 반응형 css 기본 구조 작성

---
+++ client/src/component/chefAllCourse.js
@@ -0,0 +1,7 @@
+import { ChefAllCourseInfo } from '../styled/styleChefInfo';
+
+function ChefAllCourse() {
+  return <ChefAllCourseInfo>this is chef course info</ChefAllCourseInfo>;
+}
+
+export default ChefAllCourse;
--- client/src/pages/chefInfo.js
+++ client/src/pages/chefInfo.js
@@ -1,14 +1,51 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ChefInfoGrid } from '../styled/styleChefInfo';
+import ChefAllInfo from '../component/chefAllInfo';
+import ChefAllCourse from '../component/chefAllCourse';
+import ChefAllReview from '../component/chefAllReview';
+import {
+  ChefInfoGrid,
+  ChefInfoDesc,
+  ChefInformation,
+  ChefWrapBox,
+} from '../styled/styleChefInfo';
 
 function ChefInfo() {
+  const [chefInfoIdx, setChefInfoIdx] = useState(0);
+
   return (
     <ChefInfoGrid>
-      <p>this is ChefInfo</p>
-      <div>
-        <p>셰프 정보</p>
-        <Link to='/reservation'>예약하러 가기</Link>
-      </div>
+      <ChefInfoDesc>
+        <h2>김코딩 셰프님의 만찬에 오신 것을 환영합니다!</h2>
+      </ChefInfoDesc>
+      <ChefInformation>
+        <ul id='chefInfoOrder'>
+          <li
+            className={chefInfoIdx === 0 ? 'chefInfoActive' : null}
+            onClick={() => setChefInfoIdx(0)}
+          >
+            셰프 소개
+          </li>
+          <li
+            className={chefInfoIdx === 1 ? 'chefInfoActive' : null}
+            onClick={() => setChefInfoIdx(1)}
+          >
+            코스
+          </li>
+          <li
+            className={chefInfoIdx === 2 ? 'chefInfoActive' : null}
+            onClick={() => setChefInfoIdx(2)}
+          >
+            리뷰
+          </li>
+        </ul>
+        <ChefWrapBox>
+          {chefInfoIdx === 0 ? <ChefAllInfo /> : null}
+          {chefInfoIdx === 1 ? <ChefAllCourse /> : null}
+          {chefInfoIdx === 2 ? <ChefAllReview /> : null}
+        </ChefWrapBox>
+        {/* <Link to='/reservation'>예약하러 가기</Link> */}
+      </ChefInformation>
     </ChefInfoGrid>
   );
 }
