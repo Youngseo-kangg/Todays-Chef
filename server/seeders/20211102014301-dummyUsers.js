456833d3cda3cad40bf4e4c29a658f1539db5137
Lee Naeun <nanni4287@gmail.com>
2021-11-10 10:18:01 +0900

    [Server] dummydata 생성

--- server/seeders/20211102014301-dummyUsers.js
+++ server/seeders/20211102014301-dummyUsers.js
@@ -56,10 +56,155 @@ module.exports = {
         {
           email: 'yangsik@gmail.com',
           password: 'yangsik12?!',
+          nickname: '양식박',
+          userImg: '',
+          isChef: true,
+          isOauth: true,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+
+        {
+          email: 'parkHansik@gmail.com',
+          password: 'iamChef12?!',
+          nickname: '한식박',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'leeHansik@gmail.com',
+          password: 'iamChef12?!',
+          nickname: '한식이',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'youHansik@gmail.com',
+          password: 'iamChef12?!',
+          nickname: '한식유',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'choiHansik@gmail.com',
+          password: 'iamChef12?!',
+          nickname: '한식최',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'LeeYangsik@gmail.com',
+          password: 'yangsik12?!',
           nickname: '양식이',
           userImg: '',
           isChef: true,
           isOauth: true,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'kimYangsik@gmail.com',
+          password: 'yangsik12?!',
+          nickname: '양식김',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'yangsik@gmail.com',
+          password: 'choiyangsik12?!',
+          nickname: '양식최',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'parkYangsik@gmail.com',
+          password: 'oYangsik12?!',
+          nickname: '양식오',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
+          createdAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+          updatedAt: new Date()
+            .toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, ''),
+        },
+        {
+          email: 'jungIlsik@gmail.com',
+          password: 'Ilsik12?!',
+          nickname: '일식정',
+          userImg: '',
+          isChef: true,
+          isOauth: false,
           createdAt: new Date()
             .toISOString()
             .replace(/T/, ' ')
