66653ded4523211dffd92ac9fa113ad718a9d7c4
Lee Naeun <nanni4287@gmail.com>
2021-11-11 15:48:32 +0900

    [Server] 셰프 리스트 요청 완료

--- server/routes/chef.js
+++ server/routes/chef.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { chefController } = require('../controllers');
 
-router.get('/cuisine', chefController.cuisine.get);
+router.get('/:id', chefController.cuisine.get);
 router.get('/', chefController.chefId.get);
 
 module.exports = router;
