6db36b5e9d0391418d720758edd71e854ed7d37a
Youngseo Kang <youngseo.kangg@gmail.com>
2021-11-11 22:13:23 +0900

    [Client] findChef axios 기본 작업 완료

--- client/src/pages/findChef.js
+++ client/src/pages/findChef.js
@@ -1,11 +1,13 @@
 import { Link, useHistory } from 'react-router-dom';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import basic_profile from '../todaysChefIMG/basic_profile.jpeg';
 import koreanFood from '../todaysChefIMG/foodKorean.png';
 import japaneseFood from '../todaysChefIMG/foodJapanese.png';
 import italianFood from '../todaysChefIMG/foodItalian.png';
 import chineseFood from '../todaysChefIMG/foodChinese.png';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { login, userStatus } from '../features/user/user';
 import {
   FindChefGrid,
   SelectCuisine,
@@ -16,20 +18,56 @@ import {
   PagenationList,
 } from '../styled/styleFindChef';
 
+require('dotenv').config();
+axios.defaults.withCredentials = true;
+
 function FindChef() {
+  const url = process.env.REACT_APP_API_URL || `http://localhost:4000`;
   const history = useHistory();
-  const [selected, setSelected] = useState('한식');
+  const dispatch = useDispatch();
+  const [selected, setSelected] = useState('한식'); // select 선택값
+  const [chefData, setChefData] = useState([]);
+  const [dataLength, setDataLength] = useState({
+    start: 0,
+    end: 7,
+  });
+  const userInfo = useSelector(userStatus);
+
   const handleSelected = (event) => {
-    if (event.target.value === 'kr') {
-      setSelected('한식');
-    } else if (event.target.value === 'jp') {
-      setSelected('일식');
-    } else if (event.target.value === 'ch') {
-      setSelected('중식');
-    } else if (event.target.value === 'eu') {
-      setSelected('양식');
+    setSelected(event.target.value); // select값 업데이트
+  }; // select에서 선택하는대로 어떤 퀴진 셰프인지 변경 + 데이터 새로 요청
+
+  const getChefList = async () => {
+    try {
+      let encodeSelected = encodeURI(encodeURIComponent(selected));
+      const result = await axios.get(
+        `${url}/chef/${encodeSelected}?startNum=0&endNum=7`
+      ); // axios 요청
+      // 1. axios 응답에 새로운 accessToken이 있는 경우 redux 업데이트
+      console.log('응답: ', result);
+      if (result.accessToken) {
+        dispatch(
+          login({
+            ...userInfo.userInfo,
+            accessToken: result.data.accessToken,
+          })
+        );
+      }
+      setChefData(result.data.data); // 2. result값으로 chefData 없데이트
+      setDataLength({
+        ...dataLength,
+        end: result.data.data.length,
+      }); // 3. result값으로 dataLength 업데이트
+    } catch (err) {
+      console.log(err);
     }
-  }; // select에서 선택하는대로 어떤 퀴진 셰프인지 변경해주는 함수
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 가장 위로 올리기
+    getChefList(); // 데이터값 가져오기
+    console.log('useEffect안에 chefData: ', chefData);
+  }, [selected]); // 로드 되자마자 + select값 변경 될때 마다 리렌더링
 
   return (
     <FindChefGrid>
@@ -73,102 +111,34 @@ function FindChef() {
               id='findChefCuisine'
               onChange={handleSelected}
             >
-              <option value='kr'>한식</option>
-              <option value='jp'>일식</option>
-              <option value='ch'>중식</option>
-              <option value='eu'>양식</option>
+              <option value='한식'>한식</option>
+              <option value='일식'>일식</option>
+              <option value='중식'>중식</option>
+              <option value='양식'>양식</option>
             </select>
           </ChefListTitleWrap>
 
           <ChefItemList>
             <ul>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>김코딩 셰프</Link>
-                </h3>
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>박코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>이코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>최코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>김코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>박코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>이코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
-              <li className='chef' onClick={() => history.push('/chefInfo')}>
-                <div className='chefPic'>
-                  <img src={basic_profile} alt='셰프 사진' />
-                </div>
-                <h3>
-                  <Link to='/chefInfo'>최코딩 셰프</Link>
-                </h3>
-
-                <span>4.9</span>
-                <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
-              </li>
+              {chefData.map((el) => {
+                return (
+                  <li
+                    className='chef'
+                    key={el.id}
+                    onClick={() => history.push('/chefInfo')}
+                  >
+                    <div className='chefPic'>
+                      <img src={basic_profile} alt='셰프 사진' />{' '}
+                      {/* img파일 지정하고 나면 basic_profile 없애고 랜더링 해주기 */}
+                    </div>
+                    <h3>
+                      <Link to='/chefInfo'>{el.chefName} 셰프</Link>
+                    </h3>
+                    <span>{el.rating}</span>
+                    <ChefStar>⭐⭐⭐⭐⭐</ChefStar>
+                  </li>
+                );
+              })}
             </ul>
           </ChefItemList>
 
