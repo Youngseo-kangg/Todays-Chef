60a54811f665e3ba8706b4dbb47fb848a22937f1
Youngseo Kang <youngseo.kangg@gmail.com>
2021-11-18 02:27:00 +0900

    [Client] form 작업 및 form 기초 css 완성

--- client/src/component/reservationDate.js
+++ client/src/component/reservationDate.js
@@ -6,10 +6,15 @@ import { ReservationWrap, ReservDateAndInfo } from '../styled/styleReservation';
 import subDays from 'date-fns/subDays';
 import { ko } from 'date-fns/esm/locale';
 import { getYear, getMonth, setHours, setMinutes } from 'date-fns';
+import { Controller } from 'react-hook-form';
 
-function ReservationDate({ setMakeReservation }) {
-  let today = new Date(); //오늘
-  // const years = range(1990, getYear(new Date()) + 1, 1);
+function ReservationDate({
+  makeReservation,
+  setMakeReservation,
+  register,
+  control,
+  errors,
+}) {
   const months = [
     '1월',
     '2월',
@@ -24,112 +29,144 @@ function ReservationDate({ setMakeReservation }) {
     '11월',
     '12월',
   ];
-  const [startDate, setStartDate] = useState(
-    setHours(setMinutes(new Date(today.setDate(today.getDate() + 2)), 0), 13)
-  ); // 이틀 후 부터 예약 가능
-  // console.log(startDate); // Fri Nov 19 2021 13:00:13 GMT+0900 (한국 표준시)
-
+  let dateFormat = 'yyyy년 MMMM dd일, aa h:mm';
   return (
-    <ReservationWrap>
+    <ReservationWrap className={makeReservation === 1 ? null : 'none'}>
       <button onClick={() => setMakeReservation(0)}>&lt;</button>
       <div className='reservScheduleAndInfo'>
         <ReservDateAndInfo>
           <h2>1. 일정 및 개인정보 작성</h2>
           <div className='reservInputWrap'>
             <div className='reservInput'>
-              <label htmlFor='dateAndTime'>예약 날짜와 시간</label>
-              <DatePicker
-                withPortal
-                locale={ko}
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                minDate={new Date()}
-                includeTimes={[
-                  setHours(setMinutes(new Date(), 0), 12),
-                  setHours(setMinutes(new Date(), 30), 12),
-                  setHours(setMinutes(new Date(), 0), 13),
-                  setHours(setMinutes(new Date(), 30), 13),
-                  setHours(setMinutes(new Date(), 0), 14),
-                  setHours(setMinutes(new Date(), 30), 14),
-                  setHours(setMinutes(new Date(), 0), 15),
-                  setHours(setMinutes(new Date(), 30), 15),
-                  setHours(setMinutes(new Date(), 0), 16),
-                  setHours(setMinutes(new Date(), 30), 16),
-                  setHours(setMinutes(new Date(), 0), 17),
-                  setHours(setMinutes(new Date(), 30), 17),
-                  setHours(setMinutes(new Date(), 0), 18),
-                  setHours(setMinutes(new Date(), 30), 18),
-                  setHours(setMinutes(new Date(), 0), 19),
-                  setHours(setMinutes(new Date(), 30), 19),
-                ]}
-                excludeDates={[new Date(), subDays(new Date(), -1)]}
-                dateFormat='yyyy년 MMMM dd일, aa h:mm'
-                showTimeSelect
-                renderCustomHeader={({
-                  date,
-                  prevMonthButtonDisabled,
-                  nextMonthButtonDisabled,
-                  decreaseMonth,
-                  increaseMonth,
-                }) => (
-                  <div
-                    style={{
-                      margin: 10,
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      fontSize: 20,
-                    }}
-                  >
-                    <div
-                      className='btn_month btn_month-prev'
-                      onClick={decreaseMonth}
-                      disabled={prevMonthButtonDisabled}
-                      style={{
-                        cursor: 'pointer',
-                      }}
-                    >
-                      &lt;
-                    </div>
-                    <div className='month-day'>
-                      {getYear(date)}.{months[getMonth(date)]}
-                    </div>
-                    <div
-                      className='btn_month btn_month-next'
-                      onClick={increaseMonth}
-                      disabled={nextMonthButtonDisabled}
-                      style={{
-                        cursor: 'pointer',
-                      }}
-                    >
-                      &gt;
-                    </div>
-                  </div>
+              <label htmlFor='reservDateAndTime'>예약 날짜와 시간</label>
+              <Controller
+                control={control}
+                name='reservDateAndTime'
+                format={dateFormat}
+                defaultValue={null}
+                render={({ field }) => (
+                  <DatePicker
+                    withPortal
+                    placeholderText='날짜를 선택해주세요.'
+                    locale={ko}
+                    selected={field.value}
+                    onChange={(e) => field.onChange(e)}
+                    minDate={new Date()}
+                    includeTimes={[
+                      setHours(setMinutes(new Date(), 0), 12),
+                      setHours(setMinutes(new Date(), 30), 12),
+                      setHours(setMinutes(new Date(), 0), 13),
+                      setHours(setMinutes(new Date(), 30), 13),
+                      setHours(setMinutes(new Date(), 0), 14),
+                      setHours(setMinutes(new Date(), 30), 14),
+                      setHours(setMinutes(new Date(), 0), 15),
+                      setHours(setMinutes(new Date(), 30), 15),
+                      setHours(setMinutes(new Date(), 0), 16),
+                      setHours(setMinutes(new Date(), 30), 16),
+                      setHours(setMinutes(new Date(), 0), 17),
+                      setHours(setMinutes(new Date(), 30), 17),
+                      setHours(setMinutes(new Date(), 0), 18),
+                      setHours(setMinutes(new Date(), 30), 18),
+                      setHours(setMinutes(new Date(), 0), 19),
+                      setHours(setMinutes(new Date(), 30), 19),
+                    ]}
+                    excludeDates={[new Date(), subDays(new Date(), -1)]}
+                    dateFormat={dateFormat}
+                    showTimeSelect
+                    renderCustomHeader={({
+                      date,
+                      prevMonthButtonDisabled,
+                      nextMonthButtonDisabled,
+                      decreaseMonth,
+                      increaseMonth,
+                    }) => (
+                      <div
+                        style={{
+                          margin: 10,
+                          display: 'flex',
+                          justifyContent: 'space-between',
+                          fontSize: 20,
+                        }}
+                      >
+                        <div
+                          className='btn_month btn_month-prev'
+                          onClick={decreaseMonth}
+                          disabled={prevMonthButtonDisabled}
+                          style={{
+                            cursor: 'pointer',
+                          }}
+                        >
+                          &lt;
+                        </div>
+                        <div className='month-day'>
+                          {getYear(date)}.{months[getMonth(date)]}
+                        </div>
+                        <div
+                          className='btn_month btn_month-next'
+                          onClick={increaseMonth}
+                          disabled={nextMonthButtonDisabled}
+                          style={{
+                            cursor: 'pointer',
+                          }}
+                        >
+                          &gt;
+                        </div>
+                      </div>
+                    )}
+                  />
                 )}
-              />
+              ></Controller>
             </div>
           </div>
 
           <div className='reservInputWrap'>
             <div className='reservInput'>
-              <label htmlFor='location'>주소</label>
-              <input type='text' name='location' placeholder='주소' />
+              <label htmlFor='reservLocation'>주소</label>
+              <input
+                type='text'
+                name='reservLocation'
+                placeholder='주소'
+                {...register('reservLocation', {
+                  required: '주소 입력이 필요합니다.',
+                })}
+              />
             </div>
+            {errors.reservLocation && (
+              <span className='reservAlert'>
+                {errors.reservLocation.message}
+              </span>
+            )}
           </div>
+
           <div className='reservInputWrap'>
             <div className='reservInput'>
-              <label htmlFor='people'>인원</label>
-              <select name='people'>
+              <label htmlFor='reservPeople'>인원</label>
+              <select name='reservPeople' {...register('reservPeople')}>
                 <option value='2'>2명</option>
                 <option value='3'>3명</option>
                 <option value='4'>4명</option>
               </select>
             </div>
+            {errors.reservPeople && (
+              <span className='reservAlert'>{errors.reservPeople.message}</span>
+            )}
           </div>
+
           <div className='reservInputWrap'>
             <div className='reservInput'>
-              <label htmlFor='mobile'>핸드폰 번호</label>
-              <input type='text' name='mobile' placeholder='핸드폰 번호' />
+              <label htmlFor='reservMobile'>핸드폰 번호</label>
+              <input
+                type='text'
+                name='reservMobile'
+                placeholder='핸드폰 번호'
+                {...register('reservMobile', {
+                  required: '핸드폰 번호 입력이 필요합니다.',
+                })}
+              />
             </div>
+            {errors.reservMobile && (
+              <span className='reservAlert'>{errors.reservMobile.message}</span>
+            )}
           </div>
         </ReservDateAndInfo>
       </div>
--- client/src/pages/reservation.js
+++ client/src/pages/reservation.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import ReservationNotice from '../component/reservationNotice';
 import ReservationDate from '../component/reservationDate';
 import ReservationInfo from '../component/reservationInfo';
@@ -10,9 +11,46 @@ import {
   ReservationGraph,
   ReservationDesc,
 } from '../styled/styleReservation';
+import { useForm } from 'react-hook-form';
+import { setHours, setMinutes } from 'date-fns';
+require('dotenv').config();
+axios.defaults.withCredentials = true;
 
 function Reservation() {
+  const url = process.env.REACT_APP_API_URL || `http://localhost:4000`;
+  let today = new Date(); //오늘
   const [makeReservation, setMakeReservation] = useState(0);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      reservDateAndTime: setHours(
+        setMinutes(new Date(today.setDate(today.getDate() + 2)), 0),
+        13
+      ),
+      reservLocation: '',
+      reservPeople: 2,
+      reservMobile: '',
+      reservFire: 1,
+      reservOven: false,
+      reservAllergy: '',
+      reservComment: '',
+    },
+  });
+  const onSubmit = (data) => {
+    console.log('reservation에서 onSubmit: ', data);
+    // axios 요청 들어갈 예정
+    setMakeReservation(3); // 다음 페이지로 넘겨주기
+  };
+  const onError = (error) => {
+    console.log('onSubmit에서 error: ', error);
+  };
+
   return (
     <ReservationGrid>
       <ReservationTitle>
@@ -30,16 +68,23 @@ function Reservation() {
         ) : null}
 
         {makeReservation === 1 || makeReservation === 2 ? (
-          <form>
-            {makeReservation === 1 ? (
-              <ReservationDate setMakeReservation={setMakeReservation} />
-            ) : null}
-            {makeReservation === 2 ? (
-              <ReservationInfo setMakeReservation={setMakeReservation} />
-            ) : null}
+          <form onSubmit={handleSubmit(onSubmit, onError)}>
+            <ReservationDate
+              makeReservation={makeReservation}
+              setMakeReservation={setMakeReservation}
+              register={register}
+              control={control}
+              errors={errors}
+            />
+            <ReservationInfo
+              makeReservation={makeReservation}
+              setMakeReservation={setMakeReservation}
+              register={register}
+              errors={errors}
+              handleSubmit={handleSubmit}
+            />
           </form>
         ) : null}
-
         {makeReservation === 3 ? (
           <ReservationPayment setMakeReservation={setMakeReservation} />
         ) : null}
