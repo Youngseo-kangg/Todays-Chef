053bec4a98714ad9165ce90fad66f85e60e4c02d
Lee Naeun <nanni4287@gmail.com>
2021-11-22 17:05:00 +0900

    [Server] review 삭제 구현 완료

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { adminController, chefController } = require('../controllers');
 
 router.get('/review/:cuisine', adminController.review.get);
+router.delete('/review', adminController.review.delete);
 
 module.exports = router;
