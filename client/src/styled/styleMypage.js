e4983a846db5a52667aaf41ba3f584deb8e7a01d
Youngseo Kang <youngseo.kangg@gmail.com>
2021-12-03 10:53:33 +0900

    [Client[ mypage review 관련 css 수정

--- client/src/styled/styleMypage.js
+++ client/src/styled/styleMypage.js
@@ -390,11 +390,11 @@ export const MypageReviewContent = styled.div`
         grid-template-rows: repeat(4, 40px);
         row-gap: 10px;
         > li {
-          display: flex;
           width: 100%;
           height: 100%;
-          justify-content: space-evenly;
-          align-items: center;
+          display: grid;
+          grid-template-columns: repeat(3, 1fr);
+          place-items: center;
           cursor: pointer;
           background-color: #dbb89a;
           border-radius: 5px;
